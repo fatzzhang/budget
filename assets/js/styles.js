f615fa21e9432ebe8a46ec96cb07426930b6aaae
fatz <fatz.zhang@gmail.com>
2018-10-30 21:25:58 +0800

    note

---
+++ assets/js/styles.js
@@ -0,0 +1,13 @@
+
+// var x = document.getElementsById("buyWT");
+var x = $("#buyWT").val();
+
+$("#buyWT").click(function(){
+  console.log($(this).val());
+});
+// console.log(1 == 1);
+// console.log(x);
+
+// document.getElementById("demo").innerHTML = x[1].innerHTML;
+
+// document.getElementById("demo").innerHTML = 55555;
