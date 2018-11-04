6b8544629ee30cf7654a1b6fe917c5febfcd6f87
fatz <fatz.zhang@gmail.com>
2018-11-04 14:21:13 +0800

    use fake data

--- modules/Item/api.php
+++ modules/Item/api.php
@@ -5,7 +5,29 @@ class aItem extends Api
 {
     public static function do_list()
     {
-        $data = mItem::lots();
-        parent::_return(0, $data);
+        // $data = mItem::lots();
+        // parent::_return(0, $data);
+        parent::_return(0, [
+            [
+                'id' => 1,
+                'name' => '手機',
+                'price' => 20000,
+                'status' => 'normal',
+                'insert_ts' => '2018-10-30 23:54:52',
+                'insert_id' => 1,
+                'update_ts' => '2018-10-30 23:54:52',
+                'update_id' => 1,
+            ],
+            [
+                'id' => 2,
+                'name' => '電腦',
+                'price' => 50000,
+                'status' => 'normal',
+                'insert_ts' => '2018-10-30 23:54:52',
+                'insert_id' => 1,
+                'update_ts' => '2018-10-30 23:54:52',
+                'update_id' => 1,
+            ],
+        ]);
     }
 }
