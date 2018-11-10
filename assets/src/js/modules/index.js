04cb830903e3457e560cffaa72d128e9ec52af59
fatz <fatz.zhang@gmail.com>
2018-11-10 23:30:24 +0800

    cleanup duplicate code

--- assets/src/js/modules/index.js
+++ assets/src/js/modules/index.js
@@ -5,36 +5,37 @@
             $.ajax({
                 url: 'http://local.budget.tw/api/item/list',
                 type: 'post',
-                success: app.index.render.list
+                success: (res) => {
+                    res.data.forEach(data => {
+                        app.index.render.list(data, $("tbody"))    
+                    })
+                    
+                }
             });
             app.index.bind();
         },
         render: {
-            list: (response) => {
-                for (var a = 0; a < response.data.length; a++) {
-                    var res1 = $('<td></td>').text(response.data[a].name);
-                    var res2 = $('<td></td>').text(response.data[a].price);
-                    var res3 = $('<td></td>').text(response.data[a].status);
-                    var res4 = $("<td><button class='btn btn-danger btn-sm'>delete</button></td>")
-                    var addres = $('<tr></tr>').append(res1, res2, res3, res4);
-                    $("tbody").append(addres);
-                }
+            list: (data, target) => {
+                const html = $(`
+                    <tr>
+                        <td>${data.name}</td>
+                        <td>${data.price}</td>
+                        <td>${data.status}</td>
+                        <td><button class='btn btn-danger btn-sm'>delete</button></td>
+                    </tr>
+                `)
+                target.append(html);
             }   
         },
         bind: () => {
-            $("#definite").click(
-                function() {
-                    var x = $("#buyWT").val();
-                    var y = $("#span").val();
-                    var z = $("#note").val();
-                    var txt1 = $("<td></td>").text(x);
-                    var txt2 = $("<td></td>").text(y);
-                    var txt3 = $("<td></td>").text(z);
-                    var txt4 = $("<td><button class='btn btn-danger btn-sm'>delete</button></td>")
-                    var tt = $("<tr></tr>").append(txt1, txt2, txt3, txt4);
-                    $("tbody").append(tt);
+            $("#definite").click(function() {
+                const data = {
+                    name: $("#buyWT").val(),
+                    price: $("#span").val(),
+                    status: $("#note").val(),
                 }
-            )
+                app.index.render.list(data, $("tbody"));
+            })
         }
     }       
 })(app, $)
